97a7e2064f7d5229d5c81835c24bf3492ce47378
gdtknight <gdtknight@naver.com>
2023-01-18 19:50:56 +0900

    #3 - React import 문 옛날 버전, JSX 사용하지 않고 구현하는 경우 어떻게 되는지

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import Expenses from './components/Expenses';
+/*
+* 옛날 버전 import 방식
+* import React from 'react';
+* 모든 파일에 import문 추가 
+*/
+import Expenses from './components/Expenses/Expenses';
+
 
 function App() {
+
   const expenses = [
     { id: 'e1', title: 'Car Insurance', amount: 394.67, date: new Date(2021, 2, 12) },
     { id: 'e2', title: 'AirPlane Insurance', amount: 10094.67, date: new Date(2031, 2, 12) },
     { id: 'e3', title: 'Boat Insurance', amount: 9394.67, date: new Date(2030, 2, 12) }
   ];
+
+  /*
+   JSX 를 사용하지 않고 직접 구현하는 경우
+
+   return React.createElement(
+     'div',
+     {},
+     React.createElement('h2', {}, "Let's get started!"),
+     React.createElement(Expenses, { items: expenses },),
+   );
+  */
+
   return (
     <div className="App">
       <h2>Let's get started!</h2>
