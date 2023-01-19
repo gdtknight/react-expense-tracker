b1cfd5f107d96cdcde429cd283bc9208e0061886
gdtknight <gdtknight@naver.com>
2023-01-19 10:39:41 +0900

    #6 - console 경고 표시 제거
    각각의 ExpenseItem은 고유한 key 값을 가져야 한다고 한다.
    
    그래서 임의로 코드를 작성해서 추가해주었다.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,8 +2,10 @@ import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
 const Expenses = (props) => {
+  let id = 1;
   const expenseItems = props.items.map((item) =>
     <ExpenseItem
+      key={id++}
       date={item.date}
       title={item.title}
       amount={item.amount}
